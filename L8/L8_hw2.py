b7c4ca5d93b7d7fafb27b05a4f39c54b83b11274
Ramilia <rd.rd@mail.ru>
2020-05-14 18:57:50 +0300

    Lesson 8. hw 2

--- L8/L8_hw2.py
+++ L8/L8_hw2.py
@@ -1,3 +1,29 @@
+import traceback
 
 
+class MyZeroDivision(Exception):
+    def __init__(self, txt):
+        self.txt = txt
 
+    def __str__(self):
+        return self.txt
+
+
+if __name__ == '__main__':
+    def incorrect(a, b):
+        if b == 0:
+            raise MyZeroDivision('Нельзя делить на ноль, даже если очень хочется!')
+        else:
+            return a / b
+
+
+    try:
+        res = incorrect(5, 1)   # или 5, 0
+    except MyZeroDivision:
+        print('Ошибка деления:\n', traceback.format_exc())
+    else:
+        print(f'результат деления: {res}')
+    finally:
+        print('Конец!')
+
+# print(MyZeroDivision)
